23923430a40c888efa394527a69e7a8598bc6757
ShehabEldeenAyman <shehab.ayman@vito.be>
2025-11-21 12:53:01 +0100

    better logs

--- filebrowser_V2.py
+++ filebrowser_V2.py
@@ -6,7 +6,7 @@ import time
 from datetime import date, timedelta
 
 
-directory = "./data/"
+directory = "data/"
 AS = Namespace("https://www.w3.org/ns/activitystreams#")
 LDES = Namespace("https://w3id.org/ldes#")
 TREE = Namespace("https://w3id.org/tree#")
@@ -20,9 +20,13 @@ def delete_ldes_files():
 
 def create_ldes_files():
     for root, dirs, files in os.walk(directory):
-        print("Current folder:", root)
+        root = Path(root).as_posix()
+        
+        #print("Current folder:", root)
+        write_log(f"Current folder: {root} \n")
         path = Path(root)
-        print("Last Folder", path.parts[-1]) #File name maybe
+        #print("Last Folder", path.parts[-1]) #File name maybe
+        write_log(f"Last Folder {path.parts[-1]} \n")
         #print("Subfolders:", dirs)
         #print("Subfolders:\n " + "\n ".join(dirs))
         # with open(os.path.join(root,f"{path.parts[-1]}.ttl"),'w') as file:
@@ -31,17 +35,33 @@ def create_ldes_files():
         
         
         for d in dirs:
-            print(" Subfolder:", d)
+            #print(" Subfolder:", d)
+            write_log(f"Subfolder: {d} \n")
+            bn_ge = BNode()
+            bn_lt = BNode()
+            temp_graph.add((bn_ge, RDF.type, TREE.GreaterThanOrEqualToRelation))
+            temp_graph.add((bn_ge, TREE.node, URIRef(f"{eventstream_uri}{root}/{d}")))
+            temp_graph.add((bn_ge, TREE.path, AS.published))
+            #still missing the date time value here
+
+            temp_graph.add((bn_lt, RDF.type, TREE.LessThanRelation))
+            temp_graph.add((bn_lt, TREE.node, URIRef(f"{eventstream_uri}{root}/{d}")))
+            temp_graph.add((bn_lt, TREE.path, AS.published))
+            #still missing the date time value here
+
             
                 #this is where we add actual metadata about the subfolder
                 #file.write(f"{d}\n")
         
         with open(os.path.join(root,f"{path.parts[-1]}.ttl"),'a') as file: # we should move the with open with file write to after the for loop. the for loop will only creat the greater than less than relations. It will add them to the base graph initialized before the loop, then it will be added to the graph and written after the graph.
-                print(f" Writing to file: {os.path.join(root,f'{path.parts[-1]}.ttl')}")
+                #print(f" Writing to file: {os.path.join(root,f'{path.parts[-1]}.ttl')}")
+                write_log(f" Writing to file: {Path(os.path.join(root,f'{path.parts[-1]}.ttl')).as_posix()} \n")
                 file.write(temp_graph.serialize(format="trig"))
 
         #print("Files:", files)
-        print("-" * 40)
+        write_log("-" * 40)
+        write_log("\n")
+        #print("-" * 40)
 
 def create_base_graph():
     g = Graph()
@@ -65,13 +85,20 @@ def create_base_graph():
 
     return g
 
-def create_greaterthan_lessthan_relation(num):
-    bn_ge = BNode()
-    bn_lt = BNode()
+#def create_greaterthan_lessthan_relation(num):
+    
+
+
+def write_log(msg):
+    with open("logs.txt",'a') as file:
+        file.write(msg)
+def delete_log():
+    if(Path("logs.txt").exists()):
+        os.remove("logs.txt")
 
 def main():
     start_time = time.perf_counter()
-    
+    delete_log()
     delete_ldes_files()
     create_ldes_files()
     end_time = time.perf_counter()
